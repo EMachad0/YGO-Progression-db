0ad1dba746ed030490b44b5472ae98da075239e3
EMachad0 <elitonmachadod200@gmail.com>
2021-07-01 16:04:33 -0300

    tri_collection_acc

--- notebooks/collection/collection.sql
+++ notebooks/collection/collection.sql
@@ -3,8 +3,26 @@ create table collection(
     collection_cod serial unique,
     player_cod int,
     pull_cod int,
+    quantity int default 1,
     primary key (collection_cod),
     unique (player_cod, pull_cod),
     foreign key (player_cod) references player on delete cascade,
     foreign key (pull_cod) references pull on delete cascade
 );
+
+create or replace function fun_collection_acc() returns trigger as
+    $$
+    declare
+        var int;
+    begin
+        var := (select quantity from collection where player_cod=new.player_cod and pull_cod=new.pull_cod);
+        if (var is not null) then
+            update collection set quantity=var+1 where player_cod=new.player_cod and pull_cod=new.pull_cod;
+            return null;
+        else
+            return new;
+        end if;
+    end;
+    $$ language plpgsql;
+
+create trigger tri_collection_acc before insert on collection for each row execute function fun_collection_acc();
