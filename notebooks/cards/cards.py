5ed29de3b8f99b86b817555c1621274d7a30a2e9
EMachad0 <elitonmachadod200@gmail.com>
2021-07-02 16:37:19 -0300

    card insert else update

--- notebooks/cards/cards.py
+++ notebooks/cards/cards.py
@@ -4,6 +4,8 @@ from notebooks import db
 
 ygo = YGOPro()
 
+CARD_INSERT = "insert into card values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
 link_val = {'Top':1, 'Top-Left':2, 'Left':4, 'Bottom-Left':8, 'Bottom':16, 'Bottom-Right':32, 'Right':64, 'Top-Right':128}
 def link_to_mask(arr):
     if arr is None:
@@ -14,12 +16,11 @@ def link_to_mask(arr):
     return my_val
 
 def card_insert(c):
-    query = """insert into card values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on conflict (card_cod) do nothing;"""
     data = (c['id'], c['name'], c['type'], c['desc'],
             c.get('atk'), c.get('def'), c.get('level'), c.get('scale'),
             c.get('race'), c.get('attribute'), c.get('archetype'), c.get('card_images')[0]['id'],
             c.get('linkval'), link_to_mask(c.get('linkmarkers')))
-    db.make_query(query, data)
+    db.make_query(CARD_INSERT, data)
         
 
 if __name__ == '__main__':
--- notebooks/cards/cards.sql
+++ notebooks/cards/cards.sql
@@ -18,4 +18,22 @@ create table card(
     primary key (card_cod)
 );
 
+create or replace function fun_cards_ins() returns trigger as
+    $$
+    declare
+        var int;
+    begin
+        var := (select count(*) from card where card_cod=new.card_cod);
+        if (var = 0) then
+            return new;
+        else
+            update card set name=new.name, type=new.type, flavour_text=new.flavour_text, atk = new.atk, def=new.def,
+                            level=new.level, scale=new.scale, race=new.race, attribute=new.attribute, archetype=new.archetype,
+                            cod_img=new.cod_img, link_val=new.link_val, link_markers=new.link_markers
+                            where card_cod=new.card_cod;
+            return null;
+        end if;
+    end;
+    $$ language plpgsql;
 
+create trigger tri_cards_inc before insert on card for each row execute function fun_cards_ins();
